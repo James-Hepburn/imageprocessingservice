7e07ab328973015fc6bee1eb4e67043e4806614a
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-26 12:32:45 -0600

    Repositories

--- src/main/java/com/example/imageprocessingservice/repository/ImageRepository.java
+++ src/main/java/com/example/imageprocessingservice/repository/ImageRepository.java
@@ -1,4 +1,13 @@
 package com.example.imageprocessingservice.repository;
 
-public class ImageRepository {
+import com.example.imageprocessingservice.model.Image;
+import com.example.imageprocessingservice.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository <Image, Long> {
+    Page <Image> findAllByOwner (User owner, Pageable pageable);
 }
--- src/main/java/com/example/imageprocessingservice/repository/UserRepository.java
+++ src/main/java/com/example/imageprocessingservice/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.example.imageprocessingservice.repository;
 
-public class UserRepository {
+import com.example.imageprocessingservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long> {
+    Optional <User> findByUsername (String username);
+    boolean existsByUsername (String username);
 }
