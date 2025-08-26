27bec9be6453d8a125d77cdbcbdf08b276284e1a
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-26 12:04:10 -0600

    Initial commit

---
+++ src/main/java/com/example/imageprocessingservice/ImageprocessingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.imageprocessingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageprocessingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageprocessingserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imageprocessingservice/ImageprocessingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imageprocessingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageprocessingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
