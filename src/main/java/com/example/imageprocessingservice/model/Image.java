33b35e1179432c500e0e602b5f387494720a42c8
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-26 12:26:58 -0600

    Entities

--- src/main/java/com/example/imageprocessingservice/model/Image.java
+++ src/main/java/com/example/imageprocessingservice/model/Image.java
@@ -1,4 +1,39 @@
 package com.example.imageprocessingservice.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String filename;
+    private String url;
+    private String format;
+    private Long size;
+
+    @CreationTimestamp
+    private LocalDateTime uploadedAt;
+
+    @ManyToOne
+    private User owner;
+
+    private String transformations;
+
+    public Image (String filename, String url, String format, Long size, User owner, String transformations) {
+        this.filename = filename;
+        this.url = url;
+        this.format = format;
+        this.size = size;
+        this.owner = owner;
+        this.transformations = transformations;
+    }
 }
--- src/main/java/com/example/imageprocessingservice/model/User.java
+++ src/main/java/com/example/imageprocessingservice/model/User.java
@@ -1,4 +1,31 @@
 package com.example.imageprocessingservice.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @OneToMany
+    List <Image> images = new ArrayList<>();
+
+    public User (String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
